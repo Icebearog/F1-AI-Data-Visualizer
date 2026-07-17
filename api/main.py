9de0f6736a044a01e24667b651f7f61a6405e6ca
Icebearog <155736555+Icebearog@users.noreply.github.com>
2026-07-17 22:43:21 +0200

    New App!

---
+++ api/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn as uvicorn
+from openf1_client import OpenF1Client
+
+
+# Alright so this is just a simple backend for an AI F1 telemetry app, 
+# it will fetch data from OPENF1 and send it to the frontend for visualization. 
+# The goal of this tool is to show data visualization and analysis using AI because AI is the future and it's best to stay up to date with the latest technology.
+
+API_URL = "" | ''   # Placeholder for the OpenF1 API URL, you can use a .env file to store the actual API url for security reasons and link the env file.
+
+
